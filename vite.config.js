a036e19d2c741ce3a6eb210ab44078040138d2f2
CubeAndy <a072682@gmail.com>
2024-09-03 08:41:51 +0800

    作業上傳

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/HomeWork-Week-5/',
+  base: '/HomeWork-Week-6/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
